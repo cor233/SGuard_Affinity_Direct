0e7f8f518dbfa0909502ff21045452209d7270e2
aznb6666 <123441397+aznb6666@users.noreply.github.com>
2025-11-09 12:08:18 +0800

    Delete SGuard_Affinity_Direct_Downloads.ps1

--- SGuard_Affinity_Direct_Downloads.ps1
+++
@@ -1,5 +0,0 @@
-﻿if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $url = 'https://raw.githubusercontent.com/aznb6666/SGuard_Affinity_Direct/main/SGuard_Affinity_Direct.ps1 '
-    Start-Process powershell.exe "-NoExit -Command `"& {Invoke-Expression (Invoke-WebRequest -Uri '$url' -UseBasicParsing).Content}`"" -Verb RunAs
-    exit
-}
